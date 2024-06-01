e42482130cf741d0be2643d4b4288a45fb659c2e
SulAv100 <gautampardon@gmail.com>
2024-06-01 12:10:47 +0545

    feat: portfolio

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Sulav Gautam</span> As a naturally curious and idealistic individual, I've always had an insatiable appetite for discovering new technologies and creative innovations. Motivated by challenges, I continuously seek to expand my skill set and adapt to emerging industries with both technical proficiency and creative flair. My passion for design and enthusiasm for innovative ideas drive me to contribute meaningfully to the growth of any team I join within the technology sector."
+      ],
+      typeSpeed: 20,
+      contentType: 'html'
+    });
+  }, 10);
